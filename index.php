1b3d03822844fbee4dc95ed4b84d885530ffe284
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-01-30 20:24:22 +0100

    achange vote to circles

--- index.php
+++ index.php
@@ -78,6 +78,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="./css/style.css">
   <title>php-hotel</title>
 </head>
@@ -114,8 +115,15 @@
                   <div class="card-body">
                     <h4><?php echo $hotel['name'] ?></h4>
                     <p><?php echo $hotel['description'] ?></p>
-                    <p><?php echo $hotel['parking'] ? 'Parking is available': 'Parking is not available' ?></p>
-                    <p><?php echo 'Voto: '.$hotel['vote'] ?></p>
+                    <p><?php echo $hotel['parking'] ? 'Parking <span class="green-text">is</span> available': 'Parking <span class="red-text">is not</span> available' ?></p>
+                    <div class="d-flex align-items-center gap-2">
+                      <span>Voto: </span>
+                      <div class="d-flex align-items-center gap-1">
+                        <?php for($votes = 0; $votes < $hotel['vote']; ++$votes){ ?>
+                                  <i class="fa-solid fa-circle green-text"></i>
+                        <?php } ?>
+                      </div>
+                    </div>
                     <p><?php echo 'Dista dal centro '.$hotel['distance_to_center'].' km' ?></p>
                   </div>
               </div>
