262cc020be442cb32a8a09dc3e8289117a6f7c35
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-01-31 10:11:36 +0100

    changed button color

--- index.php
+++ index.php
@@ -115,7 +115,7 @@
                   <?php } ?>           
               </select>
               <input name="search" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-              <button class="btn btn-outline-success" type="submit">Search</button>
+              <button class="btn btn-outline-dark" type="submit">Search</button>
           </form>
         </div>
       </div>
